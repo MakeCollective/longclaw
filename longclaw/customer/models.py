d08f9998c3fb6c8bee27664f2b7e4e40eadce892
blake-g <blake@make.nz>
2021-04-08 16:12:31 +1200

    Create skeleton classes to build

---
+++ longclaw/customer/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    pass
+
+
+class CustomerPaymentMethod(models.Model):
+    pass
+
+
+class Subscription(models.Model):
+    pass
+
+
+class SubscriptionOrder(models.Model):
+    pass
+
+
+class SubscriptionOrderItem(models.Model):
+    pass
