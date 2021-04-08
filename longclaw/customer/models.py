57b19d0b640938c59a789cb35bcbfc5edb4a455e
blake-g <blake@make.nz>
2021-04-08 16:15:18 +1200

    Initial comment on Customer model

--- longclaw/customer/models.py
+++ longclaw/customer/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Customer(models.Model):
+    '''
+    Hold details about a user. Details include at a minimum the amount of information
+    to perform a transaction through Stripe
+    '''
     pass
 
 
