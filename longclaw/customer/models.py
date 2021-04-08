e7d8c18401283f6bcb47fa1bf0e8b0c7684ba9b2
blake-g <blake@make.nz>
2021-04-09 10:25:22 +1200

    Add initial CustomerPaymentMethod fields

--- longclaw/customer/models.py
+++ longclaw/customer/models.py
@@ -18,7 +18,7 @@ class Customer(models.Model):
     shipping_address = models.ForeignKey('shipping.Address', related_name='+', on_delete=models.SET_NULL, blank=True, null=True)
     billing_address = models.ForeignKey('shipping.Address', related_name='+', on_delete=models.SET_NULL, blank=True, null=True)
     stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
-    active_payment_method = models.ForeignKey('customer.CustomerPaymentMethod', on_delete=models.SET_NULL, blank=True, null=True)
+    active_payment_method = models.ForeignKey('customer.CustomerPaymentMethod', related_name='+', on_delete=models.SET_NULL, blank=True, null=True)
 
     # Related fields
     # CustomerPaymentMethod(s)
@@ -26,8 +26,17 @@ class Customer(models.Model):
 
 
 class CustomerPaymentMethod(models.Model):
-    pass
+    '''
+    Hold details about a Customer's (potentially) multiple payment methods (cards)
+    Must be attached to a Customer. If the attached Customer is deleted, so are it's related PaymentMethods
+    '''
+    customer = models.ForeignKey('customer.Customer', related_name='payment_methods', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, help_text='User friendly name for the payment method')
+    stripe_id = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'{self.name}'
+    
 
 class Subscription(models.Model):
     pass
