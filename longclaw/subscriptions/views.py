ee6e331e3d12aeff1a340bed49be6b3c91406ca8
blake-g <blake@make.nz>
2021-05-28 15:44:22 +1200

    Rename basket test

--- longclaw/subscriptions/views.py
+++ longclaw/subscriptions/views.py
@@ -16,6 +16,23 @@ from django.conf import settings
 ProductVariant = apps.get_model(*settings.PRODUCT_VARIANT_MODEL.split('.'))
 CURRENCY_HTML_CODE = Configuration.objects.first().currency_html_code
 
+from django.http import JsonResponse
+
+def test_add_to_basket(request):
+    bid = basket_id(request)
+
+    # get 3 random variants
+    variants = []
+    for i in range(3):
+        variants.append(ProductVariant.objects.order_by('?').first())
+    
+    import random
+    for variant in variants:
+        add_to_basket(bid, variant, random.randint(1, 5))
+    
+    # basket, bid = get_basket_items(request)
+    return JsonResponse({'success': True})
+
 
 class SubscriptionIndexView(LoginRequiredMixin, View):
     login_view = reverse_lazy('login')
