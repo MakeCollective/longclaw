1d2d24817c775941bdae040ae589564ae6a722e0
blake <blake@make.nz>
2021-07-02 12:18:06 +1200

    Add Order API custom action to list all Orders

--- longclaw/orders/api.py
+++ longclaw/orders/api.py
@@ -95,3 +95,15 @@ class OrderViewSet(viewsets.ModelViewSet):
         order = Order.objects.get(id=pk)
         order.set_status(status_code)
         return Response(self.get_serializer(order).data)
+
+    @action(detail=False, methods=['get'])
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
