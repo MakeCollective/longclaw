3c785880a970ddeb8424e189a035eb82dbc7ef5d
blake <blake@make.nz>
2021-07-05 15:13:33 +1200

    Adjust path of longclaw built script

--- longclaw/core/templatetags/longclawcore_tags.py
+++ longclaw/core/templatetags/longclawcore_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 CLIENT_PATH = os.path.join('core', 'js', 'longclawclient.bundle.js')
 VENDORS_PATH = os.path.join('core', 'js', 'vendors.bundle.js')
 
-@register.inclusion_tag("core/script.html")
+@register.inclusion_tag("longclaw/core/script.html")
 def longclaw_vendors_bundle():
     assert os.path.exists(
         os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', VENDORS_PATH)
