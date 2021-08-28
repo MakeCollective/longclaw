2555003f5fdfcfdcff7e5df4ef12d1f7fdec5a02
blake <blake@make.nz>
2021-08-28 15:47:24 +1200

    Make payment method field on PM form required

--- longclaw/subscriptions/forms.py
+++ longclaw/subscriptions/forms.py
@@ -7,3 +7,7 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['dispatch_frequency', 'dispatch_day_of_week', 'selected_payment_method'] # 'one_click_reminder', 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['selected_payment_method'].required = True
