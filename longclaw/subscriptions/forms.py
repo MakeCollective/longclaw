d900f8bbf7a00ecc4a64652c59f0f7d0e17edf53
blake <blake@make.nz>
2021-09-07 12:20:11 +1200

    Changes to subscription form fields

--- longclaw/subscriptions/forms.py
+++ longclaw/subscriptions/forms.py
@@ -6,14 +6,11 @@ class SubscriptionForm(forms.ModelForm):
 
     class Meta:
         model = Subscription
-        fields = ['dispatch_frequency', 'dispatch_day_of_week', 'selected_payment_method', 'shipping_rate'] # 'one_click_reminder', 
+        fields = ['dispatch_frequency', 'dispatch_day_of_week', 'shipping_rate'] # 'one_click_reminder', 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['dispatch_frequency'].label = 'Dispatch frequency (in weeks)'
-        self.fields['dispatch_frequency'].widget.attrs.update({'min': 1, 'max': 52})
-        
-        self.fields['selected_payment_method'].required = True
+        self.fields['dispatch_frequency'].label = 'Frequency'
 
         self.fields['shipping_rate'].label = 'Shipping option'
         self.fields['shipping_rate'].required = True
